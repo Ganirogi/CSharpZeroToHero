c1a2d38c247c77c11f0f1c0f7cbdb002e26e07e4
Dinu Giorgian Vasile <dinugv94@gmail.com>
2023-03-09 19:15:17 +0200

    Homework1

---
+++ FirstLesson/CSharpFirstProj/CSharpFirstProj/Program.cs
@@ -0,0 +1,11 @@
+﻿int BoxA, BoxB, BoxC, BoxD, BoxE, BoxF;
+BoxA = 40;
+BoxB = 4;
+BoxC = BoxA + BoxB;
+BoxD = BoxA - BoxB;
+BoxE = BoxA * BoxB;
+BoxF = BoxA / BoxB;
+Console.WriteLine("BoxC contains: " + BoxC);
+Console.WriteLine("BoxD contains: " + BoxD);
+Console.WriteLine("BoxE contains: " + BoxE);
+Console.WriteLine("BoxF contains: " + BoxF);
