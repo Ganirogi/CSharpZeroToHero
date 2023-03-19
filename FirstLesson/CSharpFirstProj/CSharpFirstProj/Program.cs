64ffec55c7ed04c138d7228746ab5728082e75f2
Ganirogi <71023150+Ganirogi@users.noreply.github.com>
2023-03-19 12:17:20 +0200

    Update Program.cs

--- FirstLesson/CSharpFirstProj/CSharpFirstProj/Program.cs
+++ FirstLesson/CSharpFirstProj/CSharpFirstProj/Program.cs
@@ -1,11 +1,13 @@
-﻿int BoxA, BoxB, BoxC, BoxD, BoxE, BoxF;
+﻿int BoxA, BoxB, BoxC, BoxD, BoxE, BoxF, BoxG;
 BoxA = 40;
 BoxB = 4;
 BoxC = BoxA + BoxB;
 BoxD = BoxA - BoxB;
 BoxE = BoxA * BoxB;
 BoxF = BoxA / BoxB;
+BoxG = BoxA % BoxB;
 Console.WriteLine("BoxC contains: " + BoxC);
 Console.WriteLine("BoxD contains: " + BoxD);
 Console.WriteLine("BoxE contains: " + BoxE);
 Console.WriteLine("BoxF contains: " + BoxF);
+Console.WriteLine("BoxF contains: " + BoxG);
